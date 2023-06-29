e721c459f6d96bf2796c8c01a98d9579a106f85d
Antoine-Lee <antoinekllee@gmail.com>
2023-06-29 23:21:13 +0800

    fix preplayer styles

--- frontend/app/screens/PrePlayerScreen.js
+++ frontend/app/screens/PrePlayerScreen.js
@@ -1,75 +1,71 @@
 import React from "react";
-
 import BackButton from "../components/BackButton.js";
-
 import {
     StyleSheet,
     View,
     Text,
     TouchableOpacity
 } from "react-native";
-
 import colours from "../config/colours.js";
 
 function PrePlayerScreen({ route, navigation }) {
     const { patient } = route.params;
 
     return (
-      <View style={styles.container}>
-        <BackButton navigation={navigation}/>
-        <View style={styles.topContainer}>
-          <Text style={styles.title}>How would you like to listen?</Text>
+        <View style={styles.container}>
+            <BackButton navigation={navigation}/>
+            <View style={styles.topContainer}>
+                <Text style={styles.title}>How would you like to listen?</Text>
+            </View>
+            <View style={styles.bottomContainer}>
+                <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("ManualPlayer", { patient })}>
+                    <Text style={styles.buttonText}>Manual</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Player", { patient })}>
+                    <Text style={styles.buttonText}>Automatic</Text>
+                </TouchableOpacity>
+            </View>
         </View>
-        <View style={styles.bottomContainear}>
-          <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("ManualPlayer", { patient })}>
-            <Text style={styles.buttonText}>Manual</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Player", { patient })}>
-            <Text style={styles.buttonText}>Automatic</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
     );
-  
-  }
-  const styles = StyleSheet.create({
+}
+
+const styles = StyleSheet.create({
     container: {
-      flex: 0.75,
-      backgroundColor: "#FFFFFF",
-      justifyContent: "center",
-      alignItems: "center",
+        flex: 1,
+        backgroundColor: "#FFFFFF",
+        justifyContent: "center",
+        alignItems: "center",
     },
     topContainer: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
+        // flex: 0.3,
+        justifyContent: "center",
+        alignItems: "center",
+        marginBottom: 100,
     },
     title: {
-      fontSize: 24,
-      fontWeight: "bold",
-      marginBottom: 24,
+        fontSize: 24,
+        fontWeight: "bold",
+        marginBottom: 24,
     },
     bottomContainer: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-      paddingHorizontal: 32,
+        // flex: 0.3,
+        justifyContent: "center",
+        alignItems: "center",
+        paddingHorizontal: 32,
     },
     button: {
-      backgroundColor: colours.primary,
-      borderRadius: 15,
-      paddingVertical: 32,
-      paddingHorizontal: 32,
-      marginBottom: 30,
-      width:250
+        backgroundColor: colours.primary,
+        borderRadius: 15,
+        paddingVertical: 32,
+        paddingHorizontal: 32,
+        marginBottom: 30,
+        width: 250
     },
     buttonText: {
-      color: "#FFFFFF",
-      fontSize: 24,
-      textAlign: "center",
+        color: "#FFFFFF",
+        fontSize: 24,
+        textAlign: "center",
     },
-  });
-  
-      
+});
 
 export default PrePlayerScreen;
