89f6b4d3594193f2492b57f76860dddfaef336a4
Antoine-Lee <antoinekllee@gmail.com>
2023-06-29 23:02:06 +0800

    better new patient styles

--- frontend/app/screens/PatientRegistration.js
+++ frontend/app/screens/PatientRegistration.js
@@ -1,47 +1,34 @@
-import React, { useState } from "react";
-import {
-    StyleSheet,
-    Text,
-    View,
-    Alert,
-    TextInput,
-    Button,
-    TouchableOpacity,
-} from "react-native";
-import RNPickerSelect from "react-native-picker-select";
-import DatePickerModal from "../components/DatePickerModal";
-import BackButton from "../components/BackButton";
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Alert, TextInput, TouchableOpacity } from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
+import DatePickerModal from '../components/DatePickerModal';
+import BackButton from '../components/BackButton';
+import colours from '../config/colours.js';
 
-import colours from "../config/colours.js";
-
-function PatientRegistration({ route, navigation }) {
-    const [name, setName] = useState("");
-    const [ethnicity, setEthnicity] = useState("");
-    const [birthdate, setBirthdate] = useState("");
-    const [birthplace, setBirthplace] = useState("");
-    const [language, setLanguage] = useState("");
-
-    const [datePickerModalActive, setDatePickerModalActive] = useState(false);
-
+const PatientRegistration = ({ route, navigation }) => {
     const countries = [ "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cabo Verde", "Cambodia", "Cameroon", "Canada", "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros", "Congo", "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Eswatini", "Ethiopia", "Fiji", "Finland", "France", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, North", "Korea, South", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Macedonia", "Norway", "Oman", "Pakistan", "Palau", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Qatar", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Sudan", "Spain", "Sri Lanka", "Sudan", "Suriname", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor-Leste", "Togo", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Yemen", "Zambia", "Zimbabwe"];
     const languages = [ "English", "Spanish", "French", "German", "Italian", "Portuguese", "Russian", "Japanese", "Chinese", "Hindi", "Arabic", "Bengali", "Punjabi", "Urdu", "Indonesian", "Swahili", "Korean", "Dutch", "Turkish", "Thai", "Vietnamese", "Greek", "Hebrew", "Polish", "Romanian", "Hungarian", "Czech", "Swedish", "Danish", "Finnish", "Norwegian", "Slovak", "Croatian", "Serbian", "Bulgarian", "Slovenian", "Estonian", "Latvian", "Lithuanian", "Maltese", "Icelandic", "Albanian", "Macedonian", "Mongolian", "Uzbek", "Kazakh", "Azerbaijani", "Georgian", "Armenian", "Belarusian", "Ukrainian", "Tamil", "Telugu", "Marathi", "Kannada", "Gujarati", "Malayalam", "Odia", "Sinhala", "Afrikaans", "Zulu", "Xhosa", "Swazi", "Sotho", "Tswana", "Ndebele", "Shona", "Amharic", "Somali", "Tigrinya", "Oromo", "Burmese", "Khmer", "Lao", "Tibetan", "Bhutanese", "Malagasy", "Tongan", "Samoan", "Fijian", "Marshallese", "Palauan", "Kinyarwanda", "Kirundi", "Chewa", "Kinyamulenge", "Bemba", "Seychellois Creole", "Chichewa"];
     const ethnicities = [ "Singaporean", "Malaysian", "Indian", "Chinese", "Eurasian", "Hokkien"];
 
-    const ethnicityOptions = [
-        "Singaporean",
-        "Malaysian",
-        "Indian",
-        "Chinese",
-        "Eurasian",
-        "Hokkien",
-    ];
+    const [formData, setFormData] = useState({
+        name: '',
+        ethnicity: '',
+        birthdate: '',
+        birthplace: '',
+        language: '',
+    });
+    const [datePickerModalActive, setDatePickerModalActive] = useState(false);
 
-    const handleSubmit = async (evt) => {
+    const handleChange = (value, name) => {
+        setFormData(prevState => ({...prevState, [name]: value}))
+    }
+
+    const handleSubmit = (evt) => {
         evt.preventDefault();
+        const { name, ethnicity, birthdate, birthplace, language } = formData;
 
-        // Check if username, password, name, age, ethnicity, birthdate, birthplace, or language are null
         if (!name || !ethnicity || !birthdate || !birthplace || !language) {
-            Alert.alert("Please fill in all fields.");
+            Alert.alert('Please fill in all fields.');
             return;
         }
 
@@ -49,24 +36,23 @@ function PatientRegistration({ route, navigation }) {
         const currentDate = new Date();
         let age = currentDate.getFullYear() - birthDateObj.getFullYear();
         const monthDiff = currentDate.getMonth() - birthDateObj.getMonth();
-        if (
-            monthDiff < 0 ||
-            (monthDiff === 0 && currentDate.getDate() < birthDateObj.getDate())
-        ) {
+        if (monthDiff < 0 || (monthDiff === 0 && currentDate.getDate() < birthDateObj.getDate())) {
             age--;
         }
 
-        const patientData = {
-            name,
-            age,
-            birthdate,
-            ethnicity,
-            birthplace,
-            language,
-        };
-
-        navigation.navigate("PatientMusicForm", { ...patientData });
+        const patientData = { name, age, birthdate, ethnicity, birthplace, language };
+        navigation.navigate('PatientMusicForm', { ...patientData });
     };
+
+    const renderPickerSelect = (placeholder, onValueChange, items) => (
+        <RNPickerSelect
+            placeholder={{ label: placeholder, value: null }}
+            onValueChange={onValueChange}
+            items={items.map(item => ({ label: item, value: item }))}
+            style={styles.pickerSelectStyles}
+            textAlign="center"
+        />
+    )
 
     return (
         <View style={styles.container}>
@@ -74,118 +60,45 @@ function PatientRegistration({ route, navigation }) {
             <View style={styles.titleContainer}>
                 <Text style={styles.title}>New Patient</Text>
             </View>
-            <Text></Text>
-            <Text></Text>
             <View style={styles.bodyContainer}>
                 <View style={styles.rightContainer}>
                     <View style={styles.inputContainer}>
                         <TextInput
-                            style={styles.dropdown}
+                            style={styles.input}
                             placeholder="Name"
                             placeholderTextColor={colours.selected}
-                            onChangeText={(name) => setName(name)}
+                            onChangeText={(value) => handleChange(value, 'name')}
                             textAlign="center"
                         />
                     </View>
 
+                    <TouchableOpacity onPress={() => setDatePickerModalActive(true)}>
+                        <View style={styles.inputContainer}>
+                            <TextInput
+                                style={styles.input}
+                                placeholder="Birthdate"
+                                placeholderTextColor={colours.selected}
+                                value={formData.birthdate}
+                                editable={false}
+                                textAlign="center"
+                                pointerEvents='none'
+                            />
+                        </View>
+                    </TouchableOpacity>
+
                     <View style={styles.inputContainer}>
-                        <TextInput
-                            style={styles.dropdown}
-                            placeholder="Birthdate"
-                            placeholderTextColor={colours.selected}
-                            value={birthdate}
-                            editable={false}
-                            textAlign="center"
-                        />
-                    </View>
-                    <Button
-                        title="Select Birthdate"
-                        color={colours.blue}
-                        onPress={() =>
-                            setDatePickerModalActive(!datePickerModalActive)
-                        }
-                    />
-                    <Text></Text>
-                    <Text></Text>
-                    <View style={styles.inputContainer}>
-                        <RNPickerSelect
-                            placeholder={{
-                                label: "Select Ethnicity",
-                                value: null,
-                            }}
-                            onValueChange={(value) => setEthnicity(value)}
-                            items={ethnicities.map((country) => ({
-                                label: country,
-                                value: country,
-                            }))}
-                            style={{
-                                inputAndroid: {
-                                    textAlign: "center",
-                                    marginTop: 13,
-                                },
-                                inputIOS: {
-                                    textAlign: "center",
-                                    marginTop: 13,
-                                },
-                            }}
-                        />
-                    </View>
-                    <View style={styles.inputContainer}>
-                        <RNPickerSelect
-                            placeholder={{
-                                label: "Select Birthplace",
-                                value: null,
-                            }}
-                            onValueChange={(value) => setBirthplace(value)}
-                            items={countries.map((country) => ({
-                                label: country,
-                                value: country,
-                            }))}
-                            style={{
-                                inputAndroid: {
-                                    textAlign: "center",
-                                    marginTop: 13,
-                                },
-                                inputIOS: {
-                                    textAlign: "center",
-                                    marginTop: 13,
-                                },
-                            }}
-                        />
+                        {renderPickerSelect("Select Ethnicity", (value) => handleChange(value, 'ethnicity'), ethnicities)}
                     </View>
 
                     <View style={styles.inputContainer}>
-                        <RNPickerSelect
-                            placeholder={{
-                                label: "Select Language",
-                                value: null,
-                            }}
-                            onValueChange={(value) => setLanguage(value)}
-                            items={languages.map((lang) => ({
-                                label: lang,
-                                value: lang,
-                            }))}
-                            style={{
-                                inputAndroid: {
-                                    textAlign: "center",
-                                    marginTop: 13,
-                                },
-                                inputIOS: {
-                                    textAlign: "center",
-                                    marginTop: 13,
-                                },
-                                placeholder: {
-                                    textAlign: "center",
-                                    marginTop: 13,
-                                },
-                            }}
-                        />
+                        {renderPickerSelect("Select Birthplace", (value) => handleChange(value, 'birthplace'), countries)}
                     </View>
 
-                    <TouchableOpacity
-                        style={styles.submitButton}
-                        onPress={handleSubmit}
-                    >
+                    <View style={styles.inputContainer}>
+                        {renderPickerSelect("Select Language", (value) => handleChange(value, 'language'), languages)}
+                    </View>
+
+                    <TouchableOpacity style={styles.submitButton} onPress={handleSubmit}>
                         <Text style={styles.submitButtonText}>Next</Text>
                     </TouchableOpacity>
                 </View>
@@ -193,8 +106,8 @@ function PatientRegistration({ route, navigation }) {
 
             {datePickerModalActive && (
                 <DatePickerModal
-                    currentDate={birthdate}
-                    setDate={setBirthdate}
+                    currentDate={formData.birthdate}
+                    setDate={(value) => handleChange(value, 'birthdate')}
                     closeModal={() => setDatePickerModalActive(false)}
                 />
             )}
@@ -214,7 +127,7 @@ const styles = StyleSheet.create({
         marginTop: 20,
     },
     bodyContainer: {
-        flex: 0.55,
+        flex: 0.45,
         flexDirection: "row",
         width: "100%",
         height: "100%",
@@ -230,16 +143,12 @@ const styles = StyleSheet.create({
         fontSize: 36,
         fontWeight: "bold",
     },
-    dropdown: {
-        alignItems: "center",
-        marginTop: 13,
-    },
     inputContainer: {
         backgroundColor: colours.secondary,
-        borderRadius: 30,
-        width: 200,
-        height: 50,
-        marginBottom: 20,
+        borderRadius: 8,
+        width: 250,
+        height: 45,
+        marginBottom: 12,
     },
     input: {
         flex: 1,
@@ -267,30 +176,15 @@ const styles = StyleSheet.create({
         fontSize: 18,
         fontWeight: "450",
     },
-    modalContainer: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "rgba(0,0,0,0.5)",
-    },
-    modalContent: {
-        backgroundColor: "white",
-        borderRadius: 10,
-        padding: 20,
-        alignItems: "center",
-        width: "80%",
-    },
-    modalTitle: {
-        fontSize: 24,
-        marginBottom: 15,
-        fontWeight: "bold",
-        color: colours.primaryText,
-    },
-    modalOption: {
-        fontSize: 18,
-        marginTop: 10,
-        marginBottom: 10,
-        color: colours.primaryText,
+    pickerSelectStyles: {
+        inputAndroid: {
+            textAlign: "center",
+            marginTop: 13,
+        },
+        inputIOS: {
+            textAlign: "center",
+            marginTop: 13,
+        }
     },
 });
 
