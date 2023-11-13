2d82c82b99e30e196d0d1bef2b04989549ded923
zany <zany.le@mission.plus>
2023-11-13 16:31:36 +0700

    fix height, width cat loader, script build

--- frontend/app/components/CustomAnimatedLoader.js
+++ frontend/app/components/CustomAnimatedLoader.js
@@ -5,27 +5,27 @@ import AnimatedLoader from 'react-native-animated-loader';
 // source animation in link: https://lottiefiles.com/search?q=loader&category=animations
 
 const CustomAnimatedLoader = ({ visible }) => {
-    return (
-        <AnimatedLoader
-            visible={visible}
-            overlayColor="rgba(255, 255, 255, 0.7)"
-            animationStyle={styles.AnimatedLoader}
-            source={require('../assets/loader/cat-loader.json')}
-            speed={1}
-        >
-            <Text style={styles.loaderText}>Loading...</Text>
-        </AnimatedLoader>
-    );
+  const { height, width } = Dimensions.get('window');
+  return (
+    <AnimatedLoader
+      visible={visible}
+      overlayColor="rgba(255, 255, 255, 0.7)"
+      animationStyle={{ height: 300, width: 300 }}
+      source={require('../assets/loader/cat-loader.json')}
+      speed={1}
+    >
+      <Text style={styles.loaderText}>Loading...</Text>
+    </AnimatedLoader>
+  );
 };
 
 export default CustomAnimatedLoader;
 
 const styles = StyleSheet.create({
-    AnimatedLoader: {},
-    loaderText: {
-        position: 'absolute',
-        fontSize: 16,
-        fontWeight: '600',
-        bottom: '38%',
-    },
+  loaderText: {
+    position: 'absolute',
+    fontSize: 16,
+    fontWeight: '600',
+    bottom: '38%',
+  },
 });
